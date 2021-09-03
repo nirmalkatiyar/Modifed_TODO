fac0d0a58a6c548ee55eab7c5d3261653fa9cb56
nirmalkatiyar <87941922+nirmalkatiyar@users.noreply.github.com>
2021-09-03 23:14:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+
+//add an item in the list//
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("enterTask").value;
+  //var inputPriorty=document.getElementById("priority").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("todo-list").appendChild(li);
+  }
+  document.getElementById("enterTask").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+//priority of list items
+
+//functionality of delete symbol
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+//done or undone
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
